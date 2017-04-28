49bccd03b35ac77ac6dbb00ae811feda006ca539
Lee Campbell <lee.ryan.campbell@gmail.com>
2017-04-28 15:22:01 +0800

    Adding IoC

---
+++ Yow.CoD.Finance.NancyWebHost/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+using Yow.CoD.Finance.Domain.Contracts;
+using Yow.CoD.Finance.Domain.Model;
+using Yow.CoD.Finance.Domain.Services;
+using Yow.CoD.Finance.NancyWebHost.Adapters;
+
+namespace Yow.CoD.Finance.NancyWebHost
+{
+    public sealed class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            container.Register<IRepository<Loan>, InMemoryRepository<Loan>>().AsSingleton();
+            container.Register<IHandler<CreateLoanCommand>, CreateLoanCommandHandler>().AsSingleton();
+        }
+    }
+}
--- Yow.CoD.Finance.NancyWebHost/LoanModule.cs
+++ Yow.CoD.Finance.NancyWebHost/LoanModule.cs
@@ -3,40 +3,44 @@ using System.Threading.Tasks;
 using Nancy;
 using Nancy.ModelBinding;
 using Yow.CoD.Finance.Domain.Contracts;
-using Yow.CoD.Finance.Domain.Model;
 using Yow.CoD.Finance.Domain.Services;
-using Yow.CoD.Finance.NancyWebHost.Adapters;
 using Yow.CoD.Finance.NancyWebHost.Models;
 
 namespace Yow.CoD.Finance.NancyWebHost
 {
-    public class LoanModule :NancyModule
+    public sealed class LoanModule :NancyModule
     {
-        public LoanModule()
+        private readonly IHandler<CreateLoanCommand> _createLoanCommandHandler;
+
+        public LoanModule(IHandler<CreateLoanCommand> createLoanCommandHandler)
         {
+            _createLoanCommandHandler = createLoanCommandHandler;
             Get["/"] = p => "curl -H \"Content - Type: application / json\" -X POST -d @CreateLoanExamplePayload.json http://localhost:64181/";
             Post["/"] = CreateLoan;
         }
 
         private async Task<HttpStatusCode> CreateLoan(object x)
         {
-            var model = this.Bind<CreateLoanModel>();
             CreateLoanCommand command;
             try
             {
+                var model = this.Bind<CreateLoanModel>();
                 command = model.ToCommand();
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 return HttpStatusCode.BadRequest;
             }
-            
 
-            //Probably replace this with DI. But only this project cares about DI now.
-            var cmdHandler = new CreateLoanCommandHandler(new InMemoryRepository<Loan>());
-            await cmdHandler.Handle(command);
-
-            return HttpStatusCode.Created;
+            try
+            {
+                await _createLoanCommandHandler.Handle(command);
+                return HttpStatusCode.Created;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.InternalServerError;
+            }
         }
     }
 }
