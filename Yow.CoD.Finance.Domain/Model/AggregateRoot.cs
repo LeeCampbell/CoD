0f8b11d9a85ed86bd0095aae3f9bbecf973b31d8
Lee Campbell <lee.ryan.campbell@gmail.com>
2017-11-08 15:08:18 +0800

    Adding doco to AggRoot

--- Yow.CoD.Finance.Domain/Model/AggregateRoot.cs
+++ Yow.CoD.Finance.Domain/Model/AggregateRoot.cs
@@ -17,10 +17,31 @@ namespace Yow.CoD.Finance.Domain.Model
         public Guid Id { get; }
         public int Version { get; private set; }
 
+        /// <summary>
+        /// Apply events to rehydrate the instance from existing events
+        /// </summary>
+        /// <param name="payload">An event to apply to the instance.</param>
         public void ApplyEvent(Event payload)
         {
             _handlers[payload.GetType()](payload);
             Version++;
+        }
+
+        /// <summary>
+        /// Get all events that have been emmitted but not yet saved.
+        /// </summary>
+        /// <returns>An array of uncommitted events.</returns>
+        public Event[] GetUncommittedEvents()
+        {
+            return _uncommittedEvents.ToArray();
+        }
+
+        /// <summary>
+        /// Acknowledges the uncommitted events as committed.
+        /// </summary>
+        public void ClearUncommittedEvents()
+        {
+            _uncommittedEvents.Clear();
         }
 
         protected void AddEvent(Event uncommittedEvent)
@@ -32,16 +53,6 @@ namespace Yow.CoD.Finance.Domain.Model
         protected void RegisterHandler<T>(Action<T> handler)
         {
             _handlers.Add(typeof(T), e=>handler((T)e));
-        }
-
-        public Event[] GetUncommittedEvents()
-        {
-            return _uncommittedEvents.ToArray();
-        }
-
-        public void ClearUncommittedEvents()
-        {
-            _uncommittedEvents.Clear();
         }
     }
 }
